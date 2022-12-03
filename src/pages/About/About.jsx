b652d47f057ce7814558b151e6a14d43d067b49a
prykdev <priyankaafssulur@gmail.com>
2022-12-03 17:35:26 +0530

    edited about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,45 +1,13 @@
-// import React from "react";
-// import { Helmet } from "react-helmet";
-
-// const About = () => {
-//   return (
-//     <div className="aboutPage">
-//       <Helmet>
-//         <title>CrypBook | About</title>
-//       </Helmet>
-//       <h3>Team Members:</h3>
-//       <br />
-//       <ul>
-//         <li>Hardik Sharma</li>
-//         <li>Mayank Kumar</li>
-//         <li>Priyaka Prasad</li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default About;
-
 import React from "react"
 import "./index.scss"
 import priyanka from "./priyanka.png"
+import mayank from "./mayank.png"
 import { AboutCard } from "../../components/ĀboutCard"
 const About = () => {
   return (
     <>
       <div className='home'>
         <div className='about-main'>
-          <AboutCard
-            name='Hardik'
-            role='Backend Developer'
-            linkedin='https://www.linkedin.com/in/tgoyal63'
-            github='https://github.com/tgoyal63'
-            email='mailto:tgoyal63@duck.com'
-            twitter='https://twitter.com/tgoyal63'
-            img={priyanka}
-            portfolio='https://flowcv.me/tgoyal63'
-            dashboard='tgoyal63'
-          />
           <AboutCard
             name='Priyanka Prasad'
             role='Web Designer'
@@ -54,12 +22,12 @@ const About = () => {
           <AboutCard
             name='Mayank'
             role='Full Stack Developer'
-            linkedin='https://www.linkedin.com/in/ayu913'
-            github='https://github.com/ayu913'
-            email='mailto:ayushsingla909@gmail.com'
-            twitter='https://twitter.com/ayu913'
-            img={priyanka}
-            portfolio='https://ayudev.vercel.app/'
+            linkedin='https://www.linkedin.com/in/mayank-1'
+            github='https://github.com/mayank-1'
+            email='mailto:mayankmk165@gmail.com'
+            twitter='https://twitter.com/mayankcodes'
+            img={mayank}
+            portfolio='https://mayank-1.github.io/'
             dashboard='ayu913'
           />
         </div>
